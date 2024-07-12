2a9f49b3dd4280616e932d9de4af20f654e03e5c
Irreflexive <33270232+Irreflexive@users.noreply.github.com>
2024-07-12 12:36:54 +0000

    initial commit

---
+++ util.hpp
@@ -0,0 +1,11 @@
+#ifndef BALATROGETHER_UTIL_H
+#define BALATROGETHER_UTIL_H
+#include <string>
+
+std::string uint64_to_string( uint64_t value ) {
+  std::ostringstream os;
+  os << value;
+  return os.str();
+}
+
+#endif
