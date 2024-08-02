fd1819130b3fd0e39c73ba36af54d8e156cde44d
Irreflexive <33270232+Irreflexive@users.noreply.github.com>
2024-08-02 12:27:41 +0000

    add whitelist to config

--- include/config.hpp
+++ include/config.hpp
@@ -17,18 +17,28 @@ class Config {
     void setTLSEnabled(bool tlsEnabled);
     bool isDebugMode();
     void setDebugMode(bool debugMode);
+
     bool isBanned(player_t p);
     bool isBanned(steamid_t steamId);
     void ban(player_t p);
     void ban(steamid_t steamId);
     void unban(player_t p);
     void unban(steamid_t steamId);
+
+    bool isWhitelisted(player_t p);
+    bool isWhitelisted(steamid_t steamId);
+    void whitelist(player_t p);
+    void whitelist(steamid_t steamId);
+    void unwhitelist(player_t p);
+    void unwhitelist(steamid_t steamId);
   private:
     void save();
     int maxPlayers = 8;
     bool tlsEnabled = true;
     steamid_list_t banned;
     bool debugMode = false;
+    bool whitelistEnabled = false;
+    steamid_list_t whitelisted;
 };
 
 #endif
