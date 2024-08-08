acf2a30819fa556a74780976de1284d5079171ee
Irreflexive <33270232+Irreflexive@users.noreply.github.com>
2024-08-08 16:00:55 +0000

    include reorganization

--- include/util.hpp
+++ include/util.hpp
@@ -2,7 +2,13 @@
 #define BALATROGETHER_UTIL_H
 
 #include <string>
+#include "json.hpp"
+
+using json = nlohmann::json;
 
 std::string getpath();
+json success(std::string cmd, json data);
+json success(std::string cmd);
+json error(std::string msg);
 
 #endif
